333cc2c68cf2333472c415d2c447f0f8165a16d2
1030907690 <1030907690@qq.com>
2020-11-09 16:05:44 +0800

    获取hostname

--- src/main/java/com/springboot/sample/controller/IndexController.java
+++ src/main/java/com/springboot/sample/controller/IndexController.java
@@ -3,12 +3,22 @@ package com.springboot.sample.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 public class IndexController {
 
     @RequestMapping("/")
     public String index() {
-        String hostName = System.getenv("COMPUTERNAME");
+        InetAddress addr = null;
+        try {
+            addr = InetAddress.getLocalHost();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        String hostName = addr.getHostName();
+        System.out.println("Local host name: "+hostName);
         return "hostName:" +hostName;
     }
 }
