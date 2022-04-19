13b49be73a20f4b7a9c63e392e89794b23893fd6
pn20120162 <pn20120162>
2022-04-19 13:53:33 +0800

    feat:条件校验增加String

--- conditional-validate-starter/src/main/java/com/conditional/validate/aspectj/action/impl/IfEqNotNullHandleImpl.java
+++ conditional-validate-starter/src/main/java/com/conditional/validate/aspectj/action/impl/IfEqNotNullHandleImpl.java
@@ -24,7 +24,7 @@ public class IfEqNotNullHandleImpl extends AbstractHandleImpl implements Validat
         ConditionalValidateField conditionalValidateField = conditionalValidateFieldInfo.getConditionalValidateField();
         // 判断该字段类型
         Class originalClz = fieldClzMap.get(conditionalValidateFieldInfo.getFieldName());
-        //TODO 只写了Integer类型的
+        //TODO 只写了Integer String类型的
         if (Integer.class.getSimpleName().equals(originalClz.getSimpleName())) {
             Expression expression = parser.parseExpression(DEFAULT_SPEL_EXPRESSION_PREFIX + paramsName[0] + SPOT + conditionalValidateFieldInfo.getFieldName());
             Integer originalValue = expression.getValue(context, Integer.class);
@@ -38,13 +38,34 @@ public class IfEqNotNullHandleImpl extends AbstractHandleImpl implements Validat
                 }
             } else {
                 // 为空的情况,有可能要求原字段为空，关联字段不能为空的情况；判断都是空就校验
-                if (StringUtils.isEmpty(conditionalValidateField.ifValue()) && StringUtils.isEmpty(originalValue)) {
+                ifEmptyUnionNotNull(conditionalValidateField, originalValue, parser, context, fieldClzMap, paramsName);
+            }
+        } else if (String.class.getSimpleName().equals(originalClz.getSimpleName())) {
+            Expression expression = parser.parseExpression(DEFAULT_SPEL_EXPRESSION_PREFIX + paramsName[0] + SPOT + conditionalValidateFieldInfo.getFieldName());
+            String originalValue = expression.getValue(context, String.class);
+            if (!StringUtils.isEmpty(conditionalValidateField.ifValue())) {
+                // 如果是相等的
+                if (conditionalValidateField.ifValue().equals(originalValue)) {
                     Expression relationExpression = parser.parseExpression(DEFAULT_SPEL_EXPRESSION_PREFIX + paramsName[0] + SPOT + conditionalValidateField.relationField());
                     String relationField = conditionalValidateField.relationField();
                     Object value = relationExpression.getValue(context, fieldClzMap.get(relationField));
                     Assert.isTrue(!StringUtils.isEmpty(value), conditionalValidateField.message());
                 }
+            } else {
+                // 为空的情况,有可能要求原字段为空，关联字段不能为空的情况；判断都是空就校验
+                ifEmptyUnionNotNull(conditionalValidateField, originalValue, parser, context, fieldClzMap, paramsName);
             }
+        } else {
+            throw new UnsupportedOperationException("暂未支持的数据类型" + originalClz.getSimpleName());
+        }
+    }
+
+    private void ifEmptyUnionNotNull(ConditionalValidateField conditionalValidateField, Object originalValue, ExpressionParser parser, EvaluationContext context, Map<String, Class> fieldClzMap, String[] paramsName) {
+        if (StringUtils.isEmpty(conditionalValidateField.ifValue()) && StringUtils.isEmpty(originalValue)) {
+            Expression relationExpression = parser.parseExpression(DEFAULT_SPEL_EXPRESSION_PREFIX + paramsName[0] + SPOT + conditionalValidateField.relationField());
+            String relationField = conditionalValidateField.relationField();
+            Object value = relationExpression.getValue(context, fieldClzMap.get(relationField));
+            Assert.isTrue(!StringUtils.isEmpty(value), conditionalValidateField.message());
         }
     }
 }
