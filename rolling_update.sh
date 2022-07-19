2f3738263004080fe98b0d304986bf5ec7be89fe
1030907690 <1030907690@qq.com>
2022-07-19 15:52:00 +0800

    ro

--- rolling_update.sh
+++ rolling_update.sh
@@ -6,3 +6,4 @@ cd target
 date=`date "+%Y%m%d%H%M%S"`
 docker build -t spring-boot-kubernetes:$date .
 kubectl set image  deployment/spring-boot-kubernetes-deployment  spring-boot-kubernetes=spring-boot-kubernetes:$date --record
+kubectl rollout status deployment/spring-boot-kubernetes-deployment
